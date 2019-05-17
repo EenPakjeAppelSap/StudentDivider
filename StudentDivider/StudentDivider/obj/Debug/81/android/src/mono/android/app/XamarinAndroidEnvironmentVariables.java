0516c79ecc793401be60d27fd58697f5b7d005a6
EenPakjeAppelSap <49155413+EenPakjeAppelSap@users.noreply.github.com>
2019-05-17 14:42:02 +0200

    New Commit

---
+++ StudentDivider/StudentDivider/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "0833ba69-9c81-4cc4-aaed-4d062a21ba14",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
